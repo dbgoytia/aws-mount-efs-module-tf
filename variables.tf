76515a32cdf52a35c7076886dda5594daaeb6479
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-27 15:34:37 -0600

    Added the input for vpc ID

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "subnets" {
   description = "Subnet for mounting the EFS volume. Preferably, an independent one."
   type        = string
 }
+
+variable "vpc_id" {
+  type = string
+  description = "The VPC ID where NFS security groups will be."
+}
